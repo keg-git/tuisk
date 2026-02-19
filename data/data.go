51a723877e147692930b70e70d2830dd00f534c2
keg-git <keg-git@proton.me>
2026-02-19 12:31:09 -0500

    added the modules I will be using

---
+++ data/data.go
@@ -0,0 +1,18 @@
+package data
+
+import(
+	"fmt"
+	"os"
+)
+
+func (m model) GetTasks() m model {
+
+}
+
+func (m model) MarkDone(id string) m model {
+
+}
+
+func (m model) CreateTask() m model {
+
+}
