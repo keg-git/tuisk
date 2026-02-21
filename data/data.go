3e1578ff905dcfd8a0b2522040f133a297abe8a7
keg-git <keg-git@proton.me>
2026-02-21 02:52:36 -0500

    added stuff to data package

--- data/data.go
+++ data/data.go
@@ -13,6 +13,12 @@ func (m model) MarkDone(id string) m model {
 
 }
 
+// we need name due date* priority* tags* description*
 func (m model) CreateTask() m model {
+
+}
+
+// gonna need the id along with what needs to be modified
+func (m model) ModifyTask() m model {
 
 }
