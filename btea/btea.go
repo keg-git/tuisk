d627b0ff27f0f4cc9bf44aa4ff2aaf059a33d01c
keg-git <keg.git@proton.me>
2026-03-21 19:46:57 -0400

    finished the new way to organize the source files and got rid of the tui directory functionality unchanged

--- btea/btea.go
+++ btea/btea.go
@@ -1,6 +1,11 @@
 package btea
 
 import (
+	// "fmt"
+	"strconv"
+	"strings"
+	"tuisk/data"
+
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -13,24 +18,69 @@ type Model struct {
 func ModelInit() Model {
 	var m Model
 	m.UpdateModel() 
-	m.Glossy()
+	m.GlossTable()
 
 	return m
 }
 
-func (m Model) Glossy() {
+func (m *Model) GlossTable() {
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(true)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	m.Table.SetStyles(s)
 
 }
 
-func (m Model) UpdateModel() {
+func (m *Model) UpdateModel() {
+
+	tasks := data.GetTasks()
+
+	columns := []table.Column{
+		{Title: "ID", Width: 3},
+		{Title: "Description", Width: 30},
+		{Title: "Priority", Width: 8},
+		{Title: "Urgency", Width: 7},
+		{Title: "tags", Width: 50},
+	}
+
+	var rows []table.Row
+	for _, task := range tasks {
+		var tags strings.Builder
+		for _, tag := range task.Tags {
+			tags.WriteString(tag + " ")
+		}
+		rows = append(rows, table.Row{
+			strconv.Itoa(task.Id),
+			task.Description,
+			task.Priority,
+			strconv.FormatFloat(task.Urgency, 'f', 2, 64),
+			tags.String(),
+		})
+	}
+
+
+	m.Table = table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(10),
+	)
 
 }
 
-func (m Model) Init() tea.Cmd {
+func (m *Model) Init() tea.Cmd {
 	return nil
 }
 
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
@@ -44,6 +94,6 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, cmd
 }
 
-func (m Model) View() string {
+func (m *Model) View() string {
 	return m.Table.View() + "\n"
 }
--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
-	"tuisk/tui"
+	// "tuisk/tui"
+	"tuisk/btea"
 	tea "github.com/charmbracelet/bubbletea"
 
 )
 
 func main() {
-	p := tea.NewProgram(tui.InitialModel())
+	m := btea.ModelInit()
+	p := tea.NewProgram(&m)
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
